0c843d40ee754158965289d8395b0d1c8d9e9b4a
Clayton Hensley <chensley56@gmail.com>
2024-03-11 21:07:43 -0700

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
   server: {
     port: 3002,
     open: true
-  }
+  },
+  resolve: {
+    alias: {
+      'postcss': path.resolve(__dirname, 'node_modules/postcss/lib/postcss.js')
+    },
+  },
 })
